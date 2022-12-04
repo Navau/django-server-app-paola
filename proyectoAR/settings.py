aed91cc5d529a9c07f90f1ec9d045ec6a324645e
Navau <milibrolunadepluton@gmail.com>
2022-12-04 19:15:34 -0400

    04/12/22

--- proyectoAR/settings.py
+++ proyectoAR/settings.py
@@ -24,7 +24,8 @@ load_dotenv(os.path.join(BASE_DIR, ".env"))
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+# SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = 'django-insecure-br4-6pry%2h!_wq7w@x$xgm2z9lt-3#7d%c*!vik6!z6a4qsma'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
